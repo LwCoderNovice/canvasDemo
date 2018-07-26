b1d82d86d46075fa646ea75e815c40094f59e395
Liam Lian <liam.lian@smith.co>
2018-07-26 21:11:47 +0800

    update

--- js/line.js
+++ js/line.js
@@ -6,7 +6,7 @@ window.onload = function() {
     
     _mainCanvas.rect(0, 0, 500, 500);// 矩形
     
-    _btn.on('click', ()=>{
+    _btn.addEventListener('click', ()=>{
         let _mainSection = document.querySelector('#canvasSection');
         let _mainCanvas = _mainSection.getContext('2d');
         _mainCanvas.fillStyle = "#f5f5f5";
