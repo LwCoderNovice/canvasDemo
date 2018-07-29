185f4ee7c66817fc20c2323c9447921bf2388821
lianwei <lianweicoder@outlook.com>
2018-07-30 01:45:51 +0800

    update

---
+++ js/arc.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+    let _btn = document.querySelector('#btn');
+    let _btnT = document.querySelector('#btnT');
+    let _btnS = document.querySelector('#btnS');
+    _btn.addEventListener('click', ()=>{
+        let _mainSection = document.querySelector('#canvasSection');
+        let _mainCanvas = _mainSection.getContext('2d');
+        _mainCanvas.beginPath();
+        _mainCanvas.arc(250, 250, 50, 0, 1.5*Math.PI, true);
+        _mainCanvas.fillStyle = 'red';
+        _mainCanvas.fill();
+    })
+    _btnT.addEventListener('click', ()=>{
+        let _mainSection = document.querySelector('#canvasSection');
+        let _mainCanvas = _mainSection.getContext('2d');
+        _mainCanvas.beginPath();
+        _mainCanvas.arc(250, 250, 50, 0, 1.5*Math.PI, false);
+        _mainCanvas.stroke();
+    })
+    _btnS.addEventListener('click', ()=>{
+        let _mainSection = document.querySelector('#canvasSection');
+        let _mainCanvas = _mainSection.getContext('2d');
+        _mainCanvas.beginPath();
+        _mainCanvas.moveTo(10, 10);
+        _mainCanvas.arcTo(250, 10, 250, 250, 100);
+        _mainCanvas.stroke();
+    })
+};
--- js/rect.js
+++ js/rect.js
@@ -2,11 +2,8 @@ window.onload = function() {
     let _mainSection = document.querySelector('#canvasSection');
     let _mainCanvas = _mainSection.getContext('2d');
 
-    _mainCanvas.fillStyle = "#f5f5f5";
-    _mainCanvas.fillRect(0, 0, 500, 500);// 矩形
-    _mainCanvas.clearRect(50, 50, 400, 400);
-    _mainCanvas.strokeStyle = "red";
-    _mainCanvas.strokeRect(100, 100, 300, 300);
-    _mainCanvas.fillStyle = "#e6e6e6";
-    _mainCanvas.fillRect(150, 150, 200, 200);// 矩形
+    _mainCanvas.fillRect(0, 0, 500, 500);// 填充矩形
+    _mainCanvas.clearRect(50, 50, 400, 400); // 清除矩形
+    _mainCanvas.strokeRect(100, 100, 300, 300); // 无填充矩形
+    _mainCanvas.fillRect(150, 150, 200, 200);
 };
